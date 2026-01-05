203d06be728ffd9dd1db1ab5f2964f3dd802ab8a
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2026-01-05 23:37:06 +0300

    vite plugin error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 
 import { defineConfig, type PluginOption } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 function resolveSrc(_path: string): string {
